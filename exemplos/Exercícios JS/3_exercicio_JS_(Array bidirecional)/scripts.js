549e620620fbe5093cdbb7cf07323bca69790b80
anacasagrandi <ana.aninha.beatriz.06@gmail.com>
2023-11-16 08:16:05 -0300

    a

--- exemplos/Exercícios JS/3_exercicio_JS_(Array bidirecional)/scripts.js
+++ exemplos/Exercícios JS/3_exercicio_JS_(Array bidirecional)/scripts.js
@@ -3,4 +3,14 @@ document.write('<h2 style="color: blue;">Resolução</h2><br>');
 
 
 // Digite a resolução do exercício aqui e visualize o resultado no navegador
+let equipesF1 = [
+    // 0           1             2
+    ['Queen', 'AC/DC', 'Red Bull'], // 0
 
+    // 0         1          2           3         4
+    ['McLaren', 'Haas', 'AlphaTauri', 'Alpine', 'Williams'], // 1
+];
+
+// Exibir Red Bull
+
+document.write(`Melhor banda de Rock n' roll: <strong>${equipesF1[0][1]} </strong> <br>`);
--- exemplos/Exercícios JS/4_exercicio_JS_(Array e Loop)/scripts.js
+++ exemplos/Exercícios JS/4_exercicio_JS_(Array e Loop)/scripts.js
@@ -3,6 +3,12 @@ document.write('<h2 style="color: blue;">Resolução</h2><br>');
 
 
 // Digite a resolução do exercício aqui e visualize o resultado no navegador
+let nomes = [
+    "Ana Beatriz", "Gabriel", "Giulia", "Lara", "Matheus"
+];
 
+let tamanho = nomes.length;
 
-
+for (let i = 0; i < tamanho; i++) {
+    document.write(` <strong>${nomes[i]}</strong><br>`);
+}
